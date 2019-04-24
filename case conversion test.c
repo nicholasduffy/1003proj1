0e5ae5b8c0b30f6bef08d26f84e7c6e1d019546f
nickuon <nickduffy1998@gmail.com>
2019-04-24 17:11:50 +1000

    Working version of caseConversion

--- case conversion test.c
+++ case conversion test.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
 
+void caseConversion(char *plainText);
+
 int main() {
-  char plainText[1024];
+  char plainText[12];
   printf("enter words\n");
-  scanf("%s\n", plainText );
+  gets(plainText);
+  caseConversion(plainText);
+  puts(plainText);
+
+  return 0;
+}
+
+void caseConversion(char *plainText) {
   int i = 0;
-  while (plainText[i] != '0') {
-    if (plainText[i] >= 97 && plainText[i] <= 122) {
+  while(plainText[i] != '\0') {
+    if (plainText[i] >= 'a' && plainText[i] <= 'z') {
       plainText[i] = (plainText[i] - 32);
     }
     i++;
   }
-printf("%s\n", plainText);
-  return 0;
 }
--- main.c
+++ main.c
@@ -7,20 +7,27 @@ Style: Stroustrup
 #include <stdio.h> // Used to take inputs from the user
 
 /*
-Function Definition
+Rotation Encryption Function
+Returns char encryptedText and uses function literal plainText[] as to not modify original plainText.
 */
-//onst char* rotationEncryption(char plainText, int keyShift);
-int main()
-{
+char rotationEncryption(char plainText[], int keyShift);
+
+/*
+Case Conversion Function:
+Returns void and uses *plainText pointer as it is modifying string.
+*/
+void caseConversion(char *plainText);
+
+int main() {
+  char plainText[1024];
+  char encryptedText[1024];
+  int keyShift;
   /*
   Basic text-based menu, printf() used multiple times to keep code clean as opposed to one extended line.
   Spaces before ordered list to indent list making the menu a little bit nicer.
   As seen in lecture with slight variation.
   */
-int menu; // Initialises an integer to be used by the menu do while loop
-int keyShift;
-char plainText[1024];
-char encryptedText[1024];
+  int menu; // Initialises an integer to be used by the menu do while loop
   do {
     printf("Please select a task:\n");
     printf(" 1. Rotation Cipher Encryption\n");
@@ -31,9 +38,10 @@ char encryptedText[1024];
       switch (menu) {
         case 1:
           printf("Enter text to be encrypted:\n");
-          scanf("%s", plainText); // Stores string in plainText
+          gets(plainText); // Stores string in plainText
+          caseConversion(plainText);
           printf("Enter enter an integer to rotate the cipher by:\n");
-          scanf("%d\n", keyShift);
+          scanf("%d\n", &keyShift);
           keyShift = (keyShift % 26);
           /*If user enters value > 25 key shift value is equivalent to if cipher made multiple rotations.
           Eg. if user enter "26" then cipher rotation is equivalent to a full rotation such that 26 = 0 and text is unchanged.
@@ -53,24 +61,31 @@ char encryptedText[1024];
   return 0;
 }
 
-//void rotationEncryption() {
-  /*
-  This while loop converts lowercase to uppercase by testing each value in the string.
-  If the tested value is within the value range of lowercase ASCII characters then 32 is subtracted from it.
-  32 is the differncqe between the uppercase and lowercase ASCII equivalents.
-  Eg. 'a' = 97 and 'A' = 65 hence 97 - 32 = 65
+/*
+Case Conversion Function:
+Uses a while loop to convert lowercase to uppercase by testing each value in the string.
+If the tested value is within the literal value range of lowercase ASCII characters then 32 is subtracted from it.
+32 is the differncqe between the uppercase and lowercase ASCII equivalents.
+Eg. 'a' = 97 and 'A' = 65 hence 97 - 32 = 65
+*/
 
-  while (plainText[i] != '/0') {
-    if (plainText[i] >= 97 && plainText[i] <= 122) {
-      plainText[i] = (plainText[i] - 32)
+void caseConversion(char *plainText) {
+  int i = 0;
+  while(plainText[i] != '\0') {
+    if (plainText[i] >= 'a' && plainText[i] <= 'z') {
+      plainText[i] = (plainText[i] - 32);
     }
+    i++;
   }
+}
 
+char rotationEncryption(char plainText[], int keyShift) {
+/*
   store in array
   create for loop
   if ascii value is capital run formula
   else if ascii value is lowercase convert then run formula
   else plain[i] = encrypted[i]
-  */
+*/
 
 //}
