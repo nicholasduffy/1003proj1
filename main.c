fa968f530d6209a9e73efab6c22d83d4d884b9ee
nickuon <nickduffy1998@gmail.com>
2019-04-24 23:13:05 +1000

    Added file IO

--- main.c
+++ main.c
@@ -7,26 +7,44 @@ Style: Stroustrup
 #include <stdio.h> // Used to take inputs from the user
 
 /*
-Rotation Encryption Function
+Write Output Function:
+Opens output.txt and writes string encryptedText.
+*/
+
+int writeOutput();
+
+/*
+Read Input Function:
+Opens input.txt and stores it in plainText as a string.
+*/
+
+int readInput();
+
+/*
+Rotation Encryption Function:
 Returns char encryptedText and uses function literal plainText[] as to not modify original plainText.
 */
+
 void rotationEncryption(char plainText[],char *encryptedText, int keyShift);
 
 /*
 Case Conversion Function:
 Returns void and uses *plainText pointer as it is modifying string.
 */
+
 void caseConversion(char *plainText);
 
 int main() {
   char plainText[1024];
   char encryptedText[1024];
   int keyShift;
+
   /*
   Basic text-based menu, printf() used multiple times to keep code clean as opposed to one extended line.
   Spaces before ordered list to indent list making the menu a little bit nicer.
   As seen in lecture with slight variation.
   */
+
   int menu; // Initialises an integer to be used by the menu do while loop
   do {
     printf("Please select a task:\n");
@@ -37,9 +55,11 @@ int main() {
     scanf("%d", &menu); // Note: \n is not used here it creates an issue where the while condition must be false twice before exiting
       switch (menu) {
         case 1:
-          printf("Enter text to be encrypted:\n");
-          scanf("%s", plainText);// Stores string in plainText
+          printf("Text to be encrypted will now be read from input.txt ...\n");
+          readInput(plainText);
           caseConversion(plainText);
+          printf("Text found:");
+          puts(plainText);
           printf("Enter enter an integer to rotate the cipher by:\n");
           scanf("%d", &keyShift);
           keyShift = (keyShift % 26);
@@ -47,7 +67,8 @@ int main() {
           Eg. if user enter "26" then cipher rotation is equivalent to a full rotation such that 26 = 0 and text is unchanged.
           */
           rotationEncryption(plainText, encryptedText, keyShift);
-          puts(encryptedText);
+          writeOutput(encryptedText);
+          printf("Encrypted text has been written to output.txt\n");
         break;
         case 2: /*rotationDecryption();*/ break;
         case 3: /*substitutionEncryption();*/ break;
@@ -58,6 +79,37 @@ int main() {
 
 
   return 0;
+}
+
+/*
+Read Input Function:
+Uses C file I/O to open a text file and store text to plainText string to be encrypted by another function.
+*/
+
+int readInput(char *plainText) { // plainText as pointer as function should modify.
+  FILE * fp;
+  fp = fopen("input.txt", "r");
+  if(fp == NULL) {
+    perror("Error opening file"); // If file cannot be opened returns an error message to the user and exits program.
+    return(-1);
+  }
+  else {
+    fgets(plainText, 1024, fp); // Stores string in plainText
+  }
+  fclose(fp);
+}
+
+int writeOutput(char encryptedText[]) { // encryptedText as literal as it shouldn't modify string
+  FILE * fp;
+  fp = fopen("output.txt", "w"); //Truncates the file length to zero if it already exists, limiting double outputs.
+  if(fp == NULL) {
+    perror("Error opening file"); // If file cannot be opened returns an error message to the user and exits program.
+    return(0);
+  }
+  else {
+    fputs(encryptedText, fp); // writes string encryptedText to output.txt .
+  }
+  fclose(fp);
 }
 
 /*
@@ -83,6 +135,11 @@ void rotationEncryption(char plainText[], char *encryptedText, int keyShift) {
   while(plainText[i] != '\0') {
     if(plainText[i] >= 65 && plainText[i] <= 90){
       encryptedText[i] = ((plainText[i] - 65 + keyShift) % 26 + 65);
+      /*
+       As 'A' has an ASCII literal value of 65 this is subtracted from the equation before the modulus operator and added back after.
+       For the purpose of the modulus operator this sets A = 0, B = 1, C = ... Z = 25.
+       This allows the equation to operate as outlined in the Assingment 1 document; "e(x) = (m + k)(mod 26)"
+      */
     }
     else {
       encryptedText[i] = plainText[i];
