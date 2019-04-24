46acee133196e1f8c70934cca9053ad09acca534
nickuon <nickduffy1998@gmail.com>
2019-04-24 15:11:35 +1000

    Correction: Text-based menu not GUI

--- main.c
+++ main.c
@@ -6,11 +6,16 @@ Style: Stroustrup
 
 #include <stdio.h> // Used to take inputs from the user
 
+/*
+Function Definition
+*/
+void rotationEncryption();
 int main()
 {
   /*
   Basic GUI, printf() used multiple times to keep code clean as opposed to one extended line.
   Spaces before ordered list to indent list making GUI a little bit nicer.
+  As seen in lecture with slight variation.
   */
 int menu; // Initialises an integer to be used by the menu do while loop
   do {
@@ -19,9 +24,9 @@ int menu; // Initialises an integer to be used by the menu do while loop
     printf(" 2. Rotation Cipher Decryption\n");
     printf(" 3. Substitution Cipher Encryption\n");
     printf(" 4. Substitution Cipher Decryption\n");
-    scanf("%d", &menu); // \n is not used here it creates an issue where the while condition must be false twice before exiting
+    scanf("%d", &menu); // Note: \n is not used here it creates an issue where the while condition must be false twice before exiting
       switch (menu) {
-        case 1: /*rotationEncryption();*/ break;
+        case 1: rotationEncryption(); break;
         case 2: /*rotationDecryption();*/ break;
         case 3: /*substitutionEncryption();*/ break;
         case 4: /*substitutionDecryption();*/ break;
@@ -31,4 +36,10 @@ int menu; // Initialises an integer to be used by the menu do while loop
 
 
   return 0;
+}
+
+void rotationEncryption() {
+  int keyShift;
+  char plainText[1024];
+  scanf("Enter text to be encrypted\n", plainText);
 }
